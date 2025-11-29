7a2943c2b63bd79707d4ca4e70e23f83cc9c4dc1
ProgramSmasher <tarun.k.baskaran@outlook.com>
2025-11-28 23:23:55 -0500

    Created homepage

---
+++ Homepage.js
@@ -0,0 +1,19 @@
+export default function Homepage() {
+  return (
+    <main
+      style={{
+        minHeight: '100vh',
+        backgroundColor: 'white',
+        padding: '24px'
+      }}
+    >
+      <h1 style={{ color: '#05102C', marginBottom: '40px' }}>
+        Placeholder text for homepage
+      </h1>
+
+      
+      
+      <div style={{ height: '1500px' }}></div>
+    </main>
+  );
+}
